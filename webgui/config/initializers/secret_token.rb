ddd646bba5d19cb5826a8420de7f1b3c1d47a6e2
amatig <amatig@gmail.com>
2010-12-23 16:46:24 +0100

    init

---
+++ webgui/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dswi::Application.config.secret_token = 'e5c4240dd8062ff0c3a916883206200ccd59d2cafb55d72b37bc7c17e8a30547ff0099e56b22a437998c30887b6718ff1da1e6bff094d2745c617e99088fb39a'
