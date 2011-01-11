0bba88a90b56e2e939216182f1f7183cb283c967
amatig <amatig@gmail.com>
2011-01-11 18:03:01 +0100

    fix rakefile

--- Rakefile
+++ Rakefile
@@ -2,18 +2,9 @@
 
 require 'core/database'
 
-task :default => :migrate
+task :default => 'db:migrate'
 
-task :environment do
-  Database::connection
-end
-
-desc "Migrazione del database attraverso gli scripts in db/migrate. Usare VERSION=x per una specifica versione."
-task :migrate => :environment do
-  Database::migrate
-end
-
-desc "Test di aggiornamento valore"
+desc "Test di aggiornamento valore user"
 task :test => :environment do
   require 'core/db/models/users.rb'
   # u = Users.all
--- core/database.rb
+++ core/database.rb
@@ -1,23 +1,22 @@
 require 'active_record'
 require 'yaml'
-require 'logger'
+# require 'logger'
 
-class Database
+namespace 'db' do
   
-  def self.connection
+  task :environment do
     path = File.expand_path('../', __FILE__)
     dbconfig = YAML::load(File.open(File.join(path, 'config/database.yml')))
     dbconfig['database'] = File.join(path, dbconfig['database'])
     ActiveRecord::Base.establish_connection(dbconfig)
-    ActiveRecord::Base.logger = Logger.new(File.open('database.log', 'a'))
-    ActiveRecord::Base.logger.level = Logger::DEBUG
+    # ActiveRecord::Base.logger = Logger.new(File.open('database.log', 'a'))
+    # ActiveRecord::Base.logger.level = Logger::DEBUG
   end
   
-  def self.migrate
+  desc "Migrazione del database attraverso gli scripts in db/migrate. Usare VERSION=x per una specifica versione."
+  task :migrate => :environment do
     path = File.expand_path('../db/migrate', __FILE__)
     ActiveRecord::Migrator.migrate(path, ENV["VERSION"] ? ENV["VERSION"].to_i : nil )
   end
   
 end
-
-
