95a4e2c5e9640496a9ace7358562993470b97620
amatig <amatig@gmail.com>
2011-01-11 11:43:26 +0100

    add activerecord

---
+++ dbm/db/migrate/001_users.rb
@@ -0,0 +1,14 @@
+class Users < ActiveRecord::Migration
+  
+  def self.up
+    create_table :users do |t|
+      t.string :name
+      t.string :cell, :limit => 15
+    end
+  end
+  
+  def self.down
+    drop_table :users
+  end
+  
+end
---
+++ dbm/db/migrate/002_prova.rb
@@ -0,0 +1,14 @@
+class Prova < ActiveRecord::Migration
+  
+  def self.up
+    create_table :prova do |t|
+      t.string :name
+      t.string :password
+    end
+  end
+  
+  def self.down
+    drop_table :prova
+  end
+  
+end
