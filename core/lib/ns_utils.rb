f7fbfe9e3875055164b2133f34cc32ca96d5d150
amatig <amatig@gmail.com>
2011-01-17 17:35:12 +0100

    fix utils

--- core/lib/ns_utils.rb
+++ core/lib/ns_utils.rb
@@ -1,15 +1,14 @@
 # More Utils
 
 # Abbellisce l'output per i comandi rake.
-# @param [String/Boolean/Hash/Array] message messaggio da formattare per un output piu' chiaro.
+# @param [String/Hash/Integer...] message messaggio da formattare per un output piu' chiaro.
 # @return [String] messaggio ben formattato per l'output da terminale.
 def output(message)
   msg = ""
-  if message.kind_of?(Hash) or message.kind_of?(ActiveRecord::Errors)
+  if message.kind_of?(Hash) or (defined?(ActiveRecord::Errors) and message.kind_of?(ActiveRecord::Errors))
     message.each { |k, v| msg += "#{k}: #{v}\n" }
   else
     msg = message
   end
   msg
 end
-
