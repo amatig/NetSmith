a0da413e1cb56e8d188bb4fca78c4ea0687898cc
Domenico Chierico <spaghetty@gmail.com>
2011-01-26 10:58:51 +0100

    fix use of Process into lib_user

--- core/backends/lib_userbackend_shell.rb
+++ core/backends/lib_userbackend_shell.rb
@@ -25,6 +25,11 @@ module UserBackend
   rescue
     nil
   end
+
+  def self.get_signed_uname()
+    Etc.getpwuid(Process.uid)
+  end
+
   #Questo metodo e' solo accessibile attraverso l'oggetto User
   def is_superuser?
     (self.userid==0)
